edc21b94801657924566d75264243592e6f0e36e
Steve Ellis <email@steveell.is>
2017-06-02 10:55:14 -0700

    move to testrpc specific set up

---
+++ README.md
@@ -0,0 +1,1 @@
+`node full_suite.js`
--- test/support/helpers.js
+++ test/support/helpers.js
@@ -1,7 +1,27 @@
 (() => {
-  eth = web3.eth;
-
   before(() => {
-    Accounts = eth.accounts.slice(1);
+    return Eth('eth_accounts')
+      .then(accounts => {
+        Accounts = accounts.slice(1);
+      });
   });
+
+  Eth = function sendEth(method, params) {
+    params = params || [];
+
+    return new Promise((resolve, reject) => {
+      provider.sendAsync({
+        jsonrpc: "2.0",
+        method: method,
+        params: params || [],
+        id: new Date().getTime()
+      }, function sendEthResponse(error, response) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response.result);
+        };
+      });
+    });
+  };
 })();
