45d8bebe0bbe5af14cadf7eabdd82898a66c9eea
Steve Ellis <email@steveell.is>
2017-07-03 01:44:26 -0700

    pull in Zeppelin ERC20 token

---
+++ contracts/token/mocks/BasicTokenMock.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.8;
+
+
+import '../BasicToken.sol';
+
+
+// mock class using BasicToken
+contract BasicTokenMock is BasicToken {
+
+  function BasicTokenMock(address initialAccount, uint initialBalance) {
+    balances[initialAccount] = initialBalance;
+    totalSupply = initialBalance;
+  }
+
+}
