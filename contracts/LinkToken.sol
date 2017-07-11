88c3227eb0648933c2e6f48682a2ed9aa2e58098
Steve Ellis <email@steveell.is>
2017-07-10 20:20:54 -0700

    improve documentation for LinkToken

--- contracts/LinkToken.sol
+++ contracts/LinkToken.sol
@@ -13,23 +13,33 @@ contract LinkToken is StandardToken, Standard223Token {
   uint8 public constant decimals = 9;
   string public constant symbol = 'LINK';
 
-
   function LinkToken()
   {
     balances[msg.sender] = totalSupply;
   }
 
+  /**
+  * @dev transfer token for a specified address.
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
   function transfer(address _to, uint _value)
   public validRecipient(_to)
   {
     super.transfer(_to, _value);
   }
 
-  function approveAndCall(address _to, uint _value, bytes _data)
+  /**
+   * @dev Aprove the passed address to transfer the specified amount of tokens, and then call the address with the given bytes.
+   * @param _recipient The address which will be allowed access to transfer the tokens.
+   * @param _value The amount of tokens allowed to be transfered from the transaction sender.
+   * @param _data The bytes to be executed at the recipient's address after transfer approval.
+   */
+  function approveAndCall(address _recipient, uint _value, bytes _data)
   public
   {
-    approve(_to, _value);
-    if (!_to.call(_data))
+    approve(_recipient, _value);
+    if (!_recipient.call(_data))
       throw;
   }
 
