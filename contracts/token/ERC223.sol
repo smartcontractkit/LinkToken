fa2d370bebea13d8c90b8e191e2126d8c9b43688
Steve Ellis <email@steveell.is>
2017-07-14 20:59:24 -0400

    remove unused contracts and tests

--- contracts/token/ERC223.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.8;
-
-import "./ERC20.sol";
-
-contract ERC223 is ERC20 {
-  function transfer(address to, uint value, bytes data) returns (bool success);
-  function unsafeTransfer(address _to, uint _value) returns (bool success);
-}
