797f1f4f5c225c5e5296874a153e749a18180f30
Steve Ellis <email@steveell.is>
2017-08-03 16:17:52 -0700

    trigger new Transfer event
    remove _data index parameter of new Transfer event to match
    https://github.com/ethereum/EIPs/issues/223

--- contracts/token/ERC223.sol
+++ contracts/token/ERC223.sol
@@ -5,5 +5,5 @@ import "./ERC20.sol";
 contract ERC223 is ERC20 {
   function transfer(address to, uint value, bytes data) returns (bool success);
 
-  event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+  event Transfer(address indexed from, address indexed to, uint value, bytes data);
 }
