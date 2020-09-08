9d2581dbee9d271d022fa52329661307fbeb5949
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-09-08 10:41:10 +0200

    Fix Travis CI config

--- truffle-config.js
+++ truffle-config.js
@@ -3,8 +3,10 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 const MNEMONIC =
   process.env.MNEMONIC ||
   'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
-const API_KEY = process.env.API_KEY
+const INFURA_API_KEY = process.env.INFURA_API_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+const walletProvider = (provider) => new HDWalletProvider(MNEMONIC, provider)
 
 module.exports = {
   //$ truffle test --network <network-name>
@@ -13,23 +15,25 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*', // match any network
+      gas: 7000000,
+      skipDryRun: true,
     },
     goerli: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${API_KEY}`),
+      provider: () => walletProvider(`https://goerli.infura.io/v3/${INFURA_API_KEY}`),
       network_id: 5,
       gas: 7000000,
       gasPrice: 10000000000, // 10 gwei
       skipDryRun: true,
     },
     maticMumbai: {
-      provider: () => new HDWalletProvider(MNEMONIC, 'https://rpc-mumbai.matic.today'),
+      provider: () => walletProvider('https://rpc-mumbai.matic.today'),
       network_id: 80001,
       gas: 7000000,
       gasPrice: 10000000000, // 10 gwei
       skipDryRun: true,
     },
     mainnet: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://mainnet.infura.io/v3/${API_KEY}`),
+      provider: () => walletProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`),
       network_id: 1,
       gas: 7000000,
       gasPrice: 10000000000, // 10 gwei
