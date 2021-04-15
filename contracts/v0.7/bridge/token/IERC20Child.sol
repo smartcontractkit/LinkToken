6d99b94ca0edd86f9b3c69a7a9a7309a8dcb3028
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2021-04-15 18:08:06 +0200

    Add LinkTokenChild contract

---
+++ contracts/v0.7/bridge/token/IERC20Child.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.6.0 <0.8.0;
+
+/* Interface Imports */
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @dev Interface of the child ERC20 token, for use on sidechains and L2 networks.
+interface IERC20Child is IERC20 {
+  /**
+   * @notice called by bridge gateway when tokens are deposited on root chain
+   * Should handle deposits by minting the required amount for the recipient
+   *
+   * @param recipient recipient address for whom deposit is being done
+   * @param amount total amount to deposit/mint
+   */
+  function deposit(address recipient, uint256 amount) external;
+
+  /**
+   * @notice called by bridge gateway when tokens are withdrawn back to root chain
+   * @dev Should burn recipient's tokens.
+   *
+   * @param amount total amount to withdraw/burn
+   */
+  function withdraw(uint256 amount) external;
+}
---
+++ contracts/v0.7/bridge/token/LinkTokenChild.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.6.0 <0.8.0;
+
+/* Interface Imports */
+import { IERC20Child } from "./IERC20Child.sol";
+
+/* Contract Imports */
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { LinkToken } from "../../../v0.6/LinkToken.sol";
+
+/// @dev Access controlled mintable & burnable LinkToken, for use on sidechains and L2 networks.
+contract LinkTokenChild is IERC20Child, AccessControl, LinkToken {
+
+  // Using this role the bridge gateway can deposit/withdraw (mint/burn)
+  bytes32 public constant BRIDGE_GATEWAY_ROLE = keccak256("BRIDGE_GATEWAY_ROLE");
+
+  /**
+   * @dev Overrides parent contract so no tokens are minted on deployment.
+   * @inheritdoc LinkToken
+   */
+  function _onCreate()
+    internal
+    override
+  {
+    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+  }
+
+  /**
+   * @dev Modifier to check access by role.
+   *
+   * @param role the required role
+   */
+  modifier only(
+    bytes32 role
+  ) {
+    require(hasRole(role, _msgSender()), "LinkTokenChild: Insufficient permissions");
+    _;
+  }
+
+  /**
+   * @dev Only callable by account with BRIDGE_GATEWAY_ROLE
+   * @inheritdoc IERC20Child
+   */
+  function deposit(
+    address recipient,
+    uint256 amount
+  )
+    external
+    override
+    virtual
+    only(BRIDGE_GATEWAY_ROLE)
+  {
+    _mint(recipient, amount);
+  }
+
+  /**
+   * @dev Only callable by account with BRIDGE_GATEWAY_ROLE
+   * @inheritdoc IERC20Child
+   */
+  function withdraw(
+    uint256 amount
+  )
+    external
+    override
+    virtual
+    only(BRIDGE_GATEWAY_ROLE)
+  {
+    _burn(_msgSender(), amount);
+  }
+}
