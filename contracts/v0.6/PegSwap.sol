9a0e59cb2510fb606840f2845998d8a58e94a6fd
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2021-05-07 19:00:38 +0200

    Include vendor contract source
    - By doing this we are trying to lower the probablity of supply chain type of attacks

--- contracts/v0.6/PegSwap.sol
+++ contracts/v0.6/PegSwap.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-import "@chainlink/contracts/src/v0.6/Owned.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "../vendor/@chainlink/contracts/0.1.7/contracts/v0.6/Owned.sol";
+import "../vendor/@openzeppelin/contracts/3.4.1/contracts/token/ERC20/ERC20.sol";
+import "../vendor/@openzeppelin/contracts/3.4.1/contracts/utils/ReentrancyGuard.sol";
+import "../vendor/@openzeppelin/contracts/3.4.1/contracts/math/SafeMath.sol";
 import "./token/ERC677Receiver.sol";
 import "./TypeAndVersionInterface.sol";
 
