5efa7dd7827e10ed03bf8841c32baca6aa6cfe38
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2021-05-12 20:25:59 +0200

    Disable static gasLimit to reproduce UNPREDICTABLE_GAS_LIMIT

--- src/optimism/index.ts
+++ src/optimism/index.ts
@@ -2,5 +2,5 @@ export * from '@chainlink/optimism-utils'
 
 // TODO: Fix ERROR { "reason":"cannot estimate gas; transaction may fail or may require manual gas limit","code":"UNPREDICTABLE_GAS_LIMIT" }
 export const TX_OVERRIDES_BUG: any = {
-  gasLimit: 8_999_999,
+  // gasLimit: 8_999_999,
 }
