ee8b691a262f7f16277bec57cb60d1c5ddb22a70
jonddon <odeyjohn4u@gmail.com>
2022-11-24 01:40:34 +0100

    removed unused version

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 export * as hardhat from './hardhat'
-export * as optimism from './optimism'
+// export * as optimism from './optimism'
 export * from './contract-defs'
 
-export const enum Versions {
-  v0_4 = '0.4',
+export const enum Versions { 
   v0_6 = '0.6',
-  v0_7 = '0.7',
+  
 }
 
 export const enum Targets {
   EVM = '', // default target
-  OVM = 'ovm',
 }
 
 export const enum Networks {
   HARDHAT = 'hardhat',
-  OPTIMISM = 'optimism', // AKA 'hardhat-optimism'
-  KOVAN_OPTIMISM = 'kovan-optimism',
   NAHMII = 'nahmii'
 }
