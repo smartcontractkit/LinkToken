96a9dd2580a41573c076fb82b68906d221555210
jonddon <odeyjohn4u@gmail.com>
2022-11-24 01:25:51 +0100

    changed openzeppelin path

--- contracts/v0.6/mocks/Token677.sol
+++ contracts/v0.6/mocks/Token677.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.6.0 <0.8.0;
 
-import "../../../vendor/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../ERC677.sol";
 
 contract Token677 is ERC20, ERC677 {
--- contracts/v0.6/token/IERC677.sol
+++ contracts/v0.6/token/IERC677.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.6.0 <0.8.0;
 
-import "../../../vendor/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IERC677 is IERC20 {
   function transferAndCall(
